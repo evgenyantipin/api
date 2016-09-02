016e07c9707da59d9f935f86827dac0cbe1ceb3a
jekaantipin <jekaantipin69@gmail.com>
2016-09-02 17:32:32 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,10 @@ set :repo_url, "git@github.com:jekaantipin/api.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/reactapi'
 
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+
 # Default value for :scm is :git
 set :scm, :git
 
@@ -34,3 +38,76 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :assets do
+  task :precompile do
+    on primary roles :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+  end
+
+  task :symlink do
+    on primary roles :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
+  end
+end
+
+
+set :unicorn_binary, "unicorn"
+
+# after 'deploy:restart', 'deploy:cleanup'
+
+namespace :unicorn do
+  desc 'Stop Unicorn'
+  task :stop do
+    on roles(:app) do
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        execute :kill, capture(:cat, fetch(:unicorn_pid))
+      end
+    end
+  end
+
+  desc 'Start Unicorn'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec unicorn -c #{fetch(:unicorn_config)} -D"
+        end
+      end
+    end
+  end
+
+  desc 'Reload Unicorn without killing master process'
+  task :reload do
+    on roles(:app) do
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        execute :kill, '-s USR2', capture(:cat, fetch(:unicorn_pid))
+      else
+        error 'Unicorn process not running'
+      end
+    end
+  end
+
+  desc 'Restart Unicorn'
+  task :restart
+  before :restart, :stop
+  before :restart, :start
+end
+
+
+after "deploy:cleanup",         "unicorn:restart"
