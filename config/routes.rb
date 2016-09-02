88fb0057fc8d3e79118e540e4b8c5ce8b168d828
jekaantipin <jekaantipin69@gmail.com>
2016-09-02 19:28:54 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#test'
+  # root 'users#test'
+  get '/', :to => redirect('/index.html')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
