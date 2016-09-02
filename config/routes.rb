6a180f2991dfa4cf3ac80c672b5f113e2da9a82c
jekaantipin <jekaantipin69@gmail.com>
2016-09-02 19:38:45 +0300

    fix static index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'users#test'
-  root :controller => 'static', :action => '/'
+  # root :controller => 'static', :action => '/'
+
+  root :to => 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
